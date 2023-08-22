e2ca8e4e855dcd57214a609a143bda8fa4584dd6
Ahmed Omar <ahmed.omar200716@gmail.com>
2023-08-22 21:17:13 +0300

    Added local storage functionality.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+const itemsArray = localStorage.getItem("items")
+  ? JSON.parse(localStorage.getItem("items"))
+  : [];
+
+const displayItems = () => {
+  let items = "";
+  for (let i = 0; i < itemsArray.length; i++) {
+    items += `<div class="task">
+                <div class="content">
+                  <input class="text" type="text" readonly="readonly" value="${itemsArray[i]}">
+                </div>
+                <div class="actions">
+                  <button class="edit">Edit</button>
+                  <button class="save hidden">Save</button>
+                  <button class="delete">Delete</button>
+                </div>
+              </div>`;
+  }
+  document.querySelector("#tasks").innerHTML = items;
+  activateDeleteListeners();
+  activateEditListeners();
+  activateSaveListeners();
+};
+
 const form = document.querySelector("#new-task-form");
 const input = document.querySelector("#new-task-input");
 const tasksContainer = document.querySelector("#tasks");
@@ -35,6 +59,13 @@ form.addEventListener("submit", (e) => {
   editAction.innerText = "Edit";
   taskActions.appendChild(editAction);
 
+  //Creates the save action
+  const saveAction = document.createElement("button");
+  saveAction.classList.add("save");
+  saveAction.classList.add("hidden");
+  saveAction.innerText = "Save";
+  taskActions.appendChild(saveAction);
+
   //Creates the delete action
   const deleteAction = document.createElement("button");
   deleteAction.classList.add("delete");
@@ -47,18 +78,60 @@ form.addEventListener("submit", (e) => {
 
   input.value = "";
 
-  editAction.addEventListener("click", (e) => {
-    if (editAction.innerText.toLowerCase() == "edit") {
-      editAction.innerText = "Save";
-      taskInput.removeAttribute("readonly");
-      taskInput.focus();
-    } else {
-      editAction.innerText = "Edit";
-      taskInput.setAttribute("readonly", "readonly");
-    }
-  });
+  addItem(taskName);
+});
 
-  deleteAction.addEventListener("click", (e) => {
-    tasksContainer.removeChild(task);
+const activateDeleteListeners = () => {
+  let deleteBtn = document.querySelectorAll(".delete");
+  deleteBtn.forEach((dB, i) => {
+    dB.addEventListener("click", () => {
+      deleteItem(i);
+    });
   });
+};
+
+const activateEditListeners = () => {
+  const editBtn = document.querySelectorAll(".edit");
+  const input = document.querySelectorAll("input.text");
+  const saveBtn = document.querySelectorAll(".save");
+  editBtn.forEach((eB, i) => {
+    eB.addEventListener("click", () => {
+      input[i].removeAttribute("readonly");
+      input[i].focus();
+      editBtn[i].classList.add("hidden");
+      saveBtn[i].classList.remove("hidden");
+    });
+  });
+};
+
+const activateSaveListeners = () => {
+  const saveBtn = document.querySelectorAll(".save");
+  const inputs = document.querySelectorAll("input.text");
+  saveBtn.forEach((sB, i) => {
+    sB.addEventListener("click", () => {
+      updateItem(inputs[i].value, i);
+    });
+  });
+};
+
+const addItem = (task) => {
+  itemsArray.push(task);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  displayItems();
+};
+
+const deleteItem = (i) => {
+  itemsArray.splice(i, 1);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  displayItems();
+};
+
+const updateItem = (text, i) => {
+  itemsArray[i] = text;
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  displayItems();
+};
+
+window.addEventListener("load", () => {
+  displayItems();
 });
